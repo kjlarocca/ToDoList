e76758bd943d3e326e030d3b96dd14a4a4ad4225
KIRSTIN <kwarren1@lssu.edu>
2023-05-23 12:32:46 -0400

    If applied, this commit will add the ability to strike-out tasks on the to-do list

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log('My code is running');
+
+const list = document.getElementById('myList');
+// Add a click event listener to the <ul> element
+list.addEventListener('click', function(event) {
+  // Check if the clicked element is an <li> element
+  if (event.target.tagName === 'LI') {
+    // Toggle the 'crossed-out' class
+    event.target.classList.toggle('crossed-out');
+  }
+});
