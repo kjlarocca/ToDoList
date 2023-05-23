6a3efb65dc8edb2c438813fa53e4d96893750645
KIRSTIN <kwarren1@lssu.edu>
2023-05-23 14:23:14 -0400

    If applied, this commit will add buttons to all fields of the to-do list and update the functionality to then cross off those items

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ list.addEventListener('click', function(event) {
     event.target.classList.toggle('crossed-out');
   }
 });
+
+// Get form and list elements
+const addItemForms = document.querySelectorAll('.addItemForm');
+const itemInputs = document.querySelectorAll('.itemInput');
+const itemLists = document.querySelectorAll('.itemList');
+
+// Add event listener to each form
+addItemForms.forEach(function(form, index) {
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    const newItem = document.createElement('li'); // Create new list item element
+    newItem.textContent = itemInputs[index].value; // Set its text content to the input value
+    itemLists[index].appendChild(newItem); // Append the new item to the list
+
+    itemInputs[index].value = ''; // Clear the input field
+  });
+});
